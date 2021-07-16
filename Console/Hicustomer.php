f1ae2b391f736480ca162e19ff70fde7eb7a5064
ankitcn <ankit@capitalnumbers.com>
2021-07-16 17:52:41 +0530

    initial commit

---
+++ Console/Hicustomer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CN\ImportCustomer\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+ 
+
+class Hicustomer extends Command
+{
+  protected function configure()
+  {
+    $this->setName('cn:customer');
+    $this->setDescription('Demo command line');
+    parent::configure();
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $output->writeln("Hi Customer");
+  }
+}
